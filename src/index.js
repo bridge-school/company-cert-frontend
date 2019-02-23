092f3dc6300bbb6c942fe521c53b128dabfdc77c
Barbara Skrela <barbara.skrela@gmail.com>
2019-02-23 09:25:03 -0500

    Set up redux structure

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './assets/index.css';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import { store } from './app/store/store';
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App /> 
+	</Provider>, 
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
