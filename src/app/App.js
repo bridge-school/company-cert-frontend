8ec22b5797ce1bf6f969161355663f63b8911586
susiekims <30587540+susiekims@users.noreply.github.com>
2019-02-24 14:52:35 -0500

    refactor div inside switch (#13)
    Fix: adding <Route/> inside switch

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import '../assets/App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import Header from '././components/Header';
+import Header from './components/Header';
 
-import routes from './router/routes';
+import Main from './components/Main'
 
 class App extends Component {
   render() {
@@ -12,15 +12,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <Header title="Home"/>
-          <Switch>
-            <div className="wrapper">
-              {
-                routes.map(({path, component, exact}, i) => {
-                  return <Route key={i} exact={exact} path={path} component={component} />
-                })
-              }
-            </div>
-          </Switch>
+          <Main />
         </div>
       </Router> 
     );
