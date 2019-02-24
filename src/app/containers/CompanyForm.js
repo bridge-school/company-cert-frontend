77d7b02afd1c761970d721dcf16641420b3fbb26
Kristyna Svecova <svecova.kristyna@gmail.com>
2019-02-24 11:41:53 -0500

    Create UI for checklist

--- src/app/containers/CompanyForm.js
+++ src/app/containers/CompanyForm.js
@@ -4,6 +4,10 @@ import FormGroup from '@material-ui/core/FormGroup';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import EventIcon from '@material-ui/icons/Event';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 /* eslint-disable */
 import { MuiPickersUtilsProvider } from 'material-ui-pickers';
@@ -11,20 +15,30 @@ import { MuiPickersUtilsProvider } from 'material-ui-pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import { DatePicker } from 'material-ui-pickers';
 
+import { checklistData } from '../../assets/checklistData';
+
 export class CompanyForm extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			selectedDate: new Date(),
+			score: 0,
 		};
 
 		// Bindings
 		this.handleDateChange = this.handleDateChange.bind(this);
+		this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
 	}
 
 	handleDateChange = date => {
 		this.setState({ selectedDate: date });
+	};
+
+	handleCheckboxChange = val => {
+		let scoreCount = this.state.score;
+		val.target.checked ? scoreCount++ : scoreCount--;
+		this.setState({ score: scoreCount });
 	};
 
 	render() {
@@ -55,6 +69,23 @@ export class CompanyForm extends Component {
 						/>
 					</MuiPickersUtilsProvider>
 				</FormGroup>
+				<FormControl margin="normal" style={{display: "block", textAlign: "left"}}>
+          <FormLabel component="legend">Company Checklist</FormLabel>
+          <FormGroup>
+						{checklistData.map((value, index) => (
+            	<FormControlLabel
+								key={index}
+            	  control={
+									<Checkbox
+										value={index.toString()}
+										onChange={this.handleCheckboxChange}
+									/>
+            	  }
+            	  label={value}
+            	/>
+						))}
+          </FormGroup>
+        </FormControl>
 
 				<Button type="submit" variant="contained" color="primary">
 					Submit
