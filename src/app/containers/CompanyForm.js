715d989be5b882968bffbac577172c7ef6450626
Kristyna Svecova <30747712+Kikisek@users.noreply.github.com>
2019-02-24 14:24:47 -0500

    Create UI for checklist (#10)
    * Create UI for checklist
    
    * Address change request

--- src/app/containers/CompanyForm.js
+++ src/app/containers/CompanyForm.js
@@ -4,12 +4,18 @@ import FormGroup from '@material-ui/core/FormGroup';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import EventIcon from '@material-ui/icons/Event';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 /* eslint-disable */
 import { MuiPickersUtilsProvider } from 'material-ui-pickers';
 // pick utils
 import DateFnsUtils from '@date-io/date-fns';
 import { DatePicker } from 'material-ui-pickers';
+
+import { checklistData } from '../../assets/checklistData';
 
 export class CompanyForm extends Component {
 	constructor(props) {
@@ -18,11 +24,13 @@ export class CompanyForm extends Component {
 		this.state = {
 			companyName: '',
 			selectedDate: new Date(),
+			score: 0,
 		};
 
 		// Bindings
 		this.handleNameChange = this.handleNameChange.bind(this);
 		this.handleDateChange = this.handleDateChange.bind(this);
+		this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
 	}
 
 	handleNameChange = event => {
@@ -31,6 +39,12 @@ export class CompanyForm extends Component {
 
 	handleDateChange = date => {
 		this.setState({ selectedDate: date });
+	};
+
+	handleCheckboxChange = val => {
+		let scoreCount = this.state.score;
+		val.target.checked ? scoreCount++ : scoreCount--;
+		this.setState({ score: scoreCount });
 	};
 
 	render() {
@@ -62,6 +76,23 @@ export class CompanyForm extends Component {
 						/>
 					</MuiPickersUtilsProvider>
 				</FormGroup>
+				<FormControl margin="normal" style={{display: "block"}}>
+          <FormLabel component="legend">Company Checklist</FormLabel>
+          <FormGroup>
+						{checklistData.map((value, index) => (
+            	<FormControlLabel
+								key={index}
+            	  control={
+									<Checkbox
+										value={index.toString()}
+										onChange={this.handleCheckboxChange}
+									/>
+            	  }
+            	  label={value}
+            	/>
+						))}
+          </FormGroup>
+        </FormControl>
 
 				<Button type="submit" variant="contained" color="primary">
 					Submit
---
+++ src/assets/checklistData.js
@@ -0,0 +1,15 @@
+export const checklistData = [
+	'accessible space was a consideration in choosing office or made minor changes to make it more accessible',
+	'gender neutral washrooms in office',
+	'have tried to de-bias your job descriptions',
+	'have at least one non-man involved in your interview process',
+	'have parental leave benefits',
+	'have at least one executive supported ERG for a marginalized group in tech or equivalent',
+	'have conducted anti-bias and/or other diversity training',
+	'provide training for managers who don\'t have precious people management experience',
+	'look outside the "traditional" CS talent pool for developers',
+	'banned "culture fit" as a reason not to hire someone',
+	'have a rubric or similar structure for your interview process',
+	'have an HR professional on staff at least part time or a plan to have one soon',
+	'allow flexible work hours',
+];
