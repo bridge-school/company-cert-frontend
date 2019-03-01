fc479642ab53625b0210428e3051a2596f0543e3
Kristyna Svecova <kristyna@rewardops.com>
2019-03-01 09:58:36 -0500

    Fix failing test

--- src/app/components/Homepage.js
+++ src/app/components/Homepage.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
+import { Provider } from 'react-redux';
 
 import CompanyForm from '../containers/CompanyForm';
+import store from '../store/store';
 
 const Homepage = () => {
   return (
     <Grid container justify="center">
       <Grid item xs={10} sm={8} md={6}>
         <h1>Bridge Company Certification</h1>
-        <CompanyForm />
+        <Provider store={store}>
+          <CompanyForm />
+        </Provider>
       </Grid>
     </Grid>
   );
