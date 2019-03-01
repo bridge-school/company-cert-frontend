8dca7114a8e81bfe01c279e0e012375a1900e2d1
Kristyna Svecova <kristyna@rewardops.com>
2019-03-01 18:31:54 -0500

    Post form data to backend and to the database

--- src/app/store/actions/submitCompanyForm.js
+++ src/app/store/actions/submitCompanyForm.js
@@ -3,14 +3,28 @@ const companyData = data => ({
   data
 });
 
-const countCompanyScore = checklistItems => checklistItems.filter(item => item).length;
+const countCompanyScore = checklistItems =>
+  checklistItems ? checklistItems.filter(item => item).length : null;
+
+const postData = (data, score) => {
+  return fetch(`companies`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...data, score }) // body data type must match "Content-Type" header
+  }).then(response => response.json()); // parses response to JSON
+};
 
 const submit = values => dispatch => {
   const score = countCompanyScore(values.companyChecklist);
   // maybe we don't need this action; it is used as a placeholder action for now
   dispatch(companyData(values));
-  console.log(values, score);
-  // make the POST request with form values here
+  postData(values, score)
+    .then(data => {
+      console.log('Success POSTing the company, id is ', data);
+    })
+    .catch(error => console.log('Error POSTing the company: ', error));
 };
 
 export default submit;
