f928de102f104948ac29bf228cd73c34c1a0ffa7
Kristyna Svecova <30747712+Kikisek@users.noreply.github.com>
2019-03-02 16:06:13 -0500

    Feature/post form data (#19)
    * Hook redux form with the company name
    
    * Make the checklist work with redux-form
    
    * Remove redundant comments
    
    * Fix failing test
    
    * Remove unused variables
    
    * Disable Submit button if fields are invalid
    
    * Add required flags to the required fields
    
    * Post form data to backend and to the database
    
    * Post also checked items from the checklist

--- src/app/store/actions/submitCompanyForm.js
+++ src/app/store/actions/submitCompanyForm.js
@@ -3,14 +3,34 @@ const companyData = data => ({
   data
 });
 
-const countCompanyScore = checklistItems => checklistItems.filter(item => item).length;
+const countCompanyScore = checklistItems =>
+  checklistItems ? checklistItems.filter(item => item).length : null;
+
+const parseCheckedIds = checklistData =>
+  checklistData
+    ? checklistData.reduce((acc, currValue, currIndex) => [...acc, currIndex], [])
+    : null;
+
+const postData = (data, score, checklist) => {
+  return fetch(`companies`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ ...data, score, checkedIds: checklist })
+  }).then(response => response.json());
+};
 
 const submit = values => dispatch => {
   const score = countCompanyScore(values.companyChecklist);
+  const parsedChecklist = parseCheckedIds(values.companyChecklist);
   // maybe we don't need this action; it is used as a placeholder action for now
   dispatch(companyData(values));
-  console.log(values, score);
-  // make the POST request with form values here
+  postData(values, score, parsedChecklist)
+    .then(data => {
+      console.log('Success POSTing the company, id is ', data);
+    })
+    .catch(error => console.log('Error POSTing the company: ', error));
 };
 
 export default submit;
