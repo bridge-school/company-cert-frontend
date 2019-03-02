6f4d0bde4866fc5afb84ff1b359e76a1168cf790
Kristyna Svecova <kristyna@rewardops.com>
2019-03-02 00:03:15 -0500

    Post also checked items from the checklist

--- src/app/components/form/CompanyChecklist.js
+++ src/app/components/form/CompanyChecklist.js
@@ -3,8 +3,7 @@ import React from 'react';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
-const CompanyChecklist = ({ label, input, meta: { touched, invalid, error } }) => {
-  // debugger
+const CompanyChecklist = ({ label, input }) => {
   return (
     <FormControlLabel
       control={<Checkbox checked={input.value ? true : false} onChange={input.onChange} />}
--- src/app/store/actions/submitCompanyForm.js
+++ src/app/store/actions/submitCompanyForm.js
@@ -6,21 +6,27 @@ const companyData = data => ({
 const countCompanyScore = checklistItems =>
   checklistItems ? checklistItems.filter(item => item).length : null;
 
-const postData = (data, score) => {
+const parseCheckedIds = checklistData =>
+  checklistData
+    ? checklistData.reduce((acc, currValue, currIndex) => [...acc, currIndex], [])
+    : null;
+
+const postData = (data, score, checklist) => {
   return fetch(`companies`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ ...data, score }) // body data type must match "Content-Type" header
-  }).then(response => response.json()); // parses response to JSON
+    body: JSON.stringify({ ...data, score, checkedIds: checklist })
+  }).then(response => response.json());
 };
 
 const submit = values => dispatch => {
   const score = countCompanyScore(values.companyChecklist);
+  const parsedChecklist = parseCheckedIds(values.companyChecklist);
   // maybe we don't need this action; it is used as a placeholder action for now
   dispatch(companyData(values));
-  postData(values, score)
+  postData(values, score, parsedChecklist)
     .then(data => {
       console.log('Success POSTing the company, id is ', data);
     })
