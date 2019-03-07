0e95707fa6ac5c3381a7dffaf47d55d384040c6f
Kristyna Svecova <30747712+Kikisek@users.noreply.github.com>
2019-03-06 21:03:49 -0500

    Fetch a single company and save in store (#27)

--- src/app/store/reducers/companyReducer.js
+++ src/app/store/reducers/companyReducer.js
@@ -1,29 +1,49 @@
-import { POST_COMPANY_SUCCESS, POST_COMPANY_FAILURE, RESET_COMPANY_DATA } from '../constants';
+import {
+  POST_COMPANY_SUCCESS,
+  POST_COMPANY_FAILURE,
+  RESET_COMPANY_DATA,
+  GET_COMPANY_DATA_SUCCESS,
+  GET_COMPANY_DATA_FAILURE
+} from '../constants';
 
 const initialState = {
   postSuccess: false,
-  companyId: null
-}
+  getCompanySuccess: false,
+  companyId: null,
+  companyData: null
+};
 
-const companyReducer = (state = initialState, {type, payload} ) => {
+const companyReducer = (state = initialState, { type, payload, data }) => {
   switch (type) {
     case POST_COMPANY_SUCCESS:
       return {
         ...state,
         postSuccess: true,
-        companyId: payload.id,
-      }
+        companyId: payload.id
+      };
     case POST_COMPANY_FAILURE:
       return {
         ...state,
         postSuccess: false,
         companyId: null
-      }
+      };
     case RESET_COMPANY_DATA:
-      return initialState
-    default: 
+      return initialState;
+    case GET_COMPANY_DATA_SUCCESS:
+      return {
+        ...state,
+        getCompanySuccess: true,
+        companyData: data
+      };
+    case GET_COMPANY_DATA_FAILURE:
+      return {
+        ...state,
+        getCompanySuccess: false,
+        companyData: null
+      };
+    default:
       return state;
   }
-}
+};
 
 export default companyReducer;
