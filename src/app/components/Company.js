18bbaeac2b2e192ace73a0a161b607a817e9420b
Kristyna Svecova <kristyna@rewardops.com>
2019-03-06 19:51:37 -0500

    Fetch a single company and save in store

--- src/app/components/Company.js
+++ src/app/components/Company.js
@@ -1,5 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import getCompanyData from '../store/actions/getCompanyData';
 
-const Company = ({match}) => <h1>Company id: {match.params.id}</h1>;
+class Company extends React.Component {
+  componentDidMount() {
+    const { match, getCompanyData } = this.props; // eslint-disable-line
+    getCompanyData(match.params.id);
+  }
 
-export default Company;
+  render() {
+    const { match, companyData } = this.props; // eslint-disable-line
+    return (
+      <div>
+        <h1>
+          Company id:
+          {match.params.id}
+        </h1>
+        {companyData && <p>Company name: {companyData.name}</p>}
+        {/* here goes the beautiful design for this page */}
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  getCompanyData: companyId => dispatch(getCompanyData(companyId))
+});
+
+const mapStateToProps = ({ companyReducer: { getCompanySuccess, companyData, companyId } }) => {
+  return {
+    getCompanySuccess,
+    companyData,
+    companyId
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Company);
