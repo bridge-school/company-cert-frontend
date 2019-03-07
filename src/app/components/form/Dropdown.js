af4259253529df12def42d23773261bbc221b7a7
Susie <susie.kim@hotmail.com>
2019-03-06 20:48:13 -0500

    clean code

--- src/app/components/form/Dropdown.js
+++ src/app/components/form/Dropdown.js
@@ -137,7 +137,7 @@ const MultiValue = props => (
     tabIndex={-1}
     label={props.children}
     className={classNames(props.selectProps.classes.chip, {
-      [props.selectProps.classes.chipFocused]: props.isFocused// true
+      [props.selectProps.classes.chipFocused]: props.isFocused
     })}
     onDelete={props.removeProps.onClick}
     deleteIcon={<CancelIcon {...props.removeProps} />}
