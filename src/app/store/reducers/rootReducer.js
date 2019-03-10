6803ff702125566328a48799139bf602c6a4b069
Kristyna Svecova <kristyna@rewardops.com>
2019-03-10 08:27:58 -0400

    Add students reducer

--- src/app/store/reducers/rootReducer.js
+++ src/app/store/reducers/rootReducer.js
@@ -4,13 +4,15 @@ import companyReducer from './companyReducer';
 import companiesReducer from './companiesReducer';
 import frontendData from './frontendDataReducer';
 import studentReducer from './studentReducer';
+import studentsReducer from './studentsReducer';
 
 const reducer = combineReducers({
   form: formReducer,
   frontendData,
   company: companyReducer,
   companies: companiesReducer,
-  student: studentReducer
+  student: studentReducer,
+  students: studentsReducer
 });
 
 export default reducer;
