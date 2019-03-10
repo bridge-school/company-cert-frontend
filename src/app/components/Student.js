8c904e450e5398d1c24a4ef55cbe6a8d182ea76a
susiekims <30587540+susiekims@users.noreply.github.com>
2019-03-10 14:23:02 -0400

    Feature/post student data (#43)
    post student form data from redux to database

--- src/app/components/Student.js
+++ src/app/components/Student.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
-
 import StudentForm from './StudentForm';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { resetAction } from '../store/actions/submitStudentForm';
 
-const Student = () => {
+const Student = ({ postSuccess, studentId, reset }) => {
+  useEffect(() => {
+    return () => {
+      reset();
+    };
+  }, []);
+
   return (
     <Grid container justify="center">
       <Grid item xs={10} sm={8} md={6}>
         <StudentForm />
+        {postSuccess && <Redirect to={`/students`} />}
       </Grid>
     </Grid>
   );
 };
 
-export default Student;
+const mapDispatchToProps = {
+  reset: resetAction
+};
+
+const mapStateToProps = ({ student: { postSuccess, studentId } }) => {
+  return {
+    postSuccess,
+    studentId
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Student);
