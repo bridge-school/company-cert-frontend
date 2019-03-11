957d607ac6e02bb40e948dc848574fdf8ebf8dfa
Barbara Skrela <barbara.skrela@gmail.com>
2019-03-11 10:21:18 -0400

    Add dropdowns to student form, save selected options to database

--- src/app/components/StudentForm.js
+++ src/app/components/StudentForm.js
@@ -5,10 +5,12 @@ import { Field, reduxForm } from 'redux-form';
 
 import StudentName from './form/NameField';
 import InterviewDate from './form/InterviewDate';
+import Dropdown from './form/Dropdown';
 import validate from '../validations';
 import submitStudentForm from '../store/actions/submitStudentForm';
 
 const Student = props => {
+  const { industry, tech } = props;
   const { handleSubmit, pristine, submitting, invalid } = props;
   return (
     <form autoComplete="off" onSubmit={handleSubmit}>
@@ -25,6 +27,20 @@ const Student = props => {
         component={InterviewDate}
         id="student-interview-date"
         label="Interview Date"
+      />
+      <Field
+        name="industry"
+        component={Dropdown}
+        label="Industries"
+        multi
+        suggestions={industry.map(item => ({ label: item.name, value: item.id }))}
+      />
+      <Field
+        name="tech"
+        component={Dropdown}
+        label="Tech Stack"
+        multi
+        suggestions={tech.map(item => ({ label: item.name, value: item.id }))}
       />
       <Button
         type="submit"
