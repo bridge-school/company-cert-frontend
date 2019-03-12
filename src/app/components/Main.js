d186ce17f1383736dd1005869d2147de53326548
susiekims <30587540+susiekims@users.noreply.github.com>
2019-03-12 17:20:48 -0400

    restoring scroll position when changing routes (#49)

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -2,15 +2,18 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import routes from '../router/routes';
 import Wrapper from './Wrapper';
+import ScrollToTop from './ScrollToTop';
 
 const Main = () => (
-  <Wrapper>
-    <Switch>
-      {routes.map(({ path, component, exact }, i) => {
-        return <Route key={i} exact={exact} path={path} component={component} />;
-      })}
-    </Switch>
-  </Wrapper>
+  <ScrollToTop>
+    <Wrapper>
+      <Switch>
+        {routes.map(({ path, component, exact }, i) => {
+          return <Route key={i} exact={exact} path={path} component={component} />;
+        })}
+      </Switch>
+    </Wrapper>
+  </ScrollToTop>
 );
 
 export default Main;
