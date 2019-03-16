96e6d6bbde4f28560d4678f69d4c399a4ee24fed
susiekims <30587540+susiekims@users.noreply.github.com>
2019-03-16 16:46:41 -0400

    Chore/reuse card and tag (#62)
    Refactor Tag and Card component

--- src/app/components/Students.js
+++ src/app/components/Students.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
-
 import getStudentsData from '../store/actions/getStudentsData';
-import StudentCard from './StudentCard';
+import Card from './Card';
+import { Link } from 'react-router-dom';
+
+const chooseRandomTags = (tagsArray, n) => {
+  // Shuffle array
+  const shuffled = tagsArray.sort(() => 0.5 - Math.random());
+
+  // Get sub-array of first n elements after shuffled
+  const selected = shuffled.slice(0, n);
+
+  return selected;
+};
 
 class Students extends React.Component {
   componentDidMount() {
@@ -14,10 +24,18 @@ class Students extends React.Component {
   render() {
     const { students } = this.props;
     return (
-      <Grid item xs={12} style={{ textAlign: 'center' }}>
+      <Grid item xs={10} sm={8} md={6}>
         <h1>Students</h1>
         {students.map(student => (
-          <StudentCard key={student.id} {...student} />
+          <Link to={`/students/${student.id}`} key={student.id}>
+            <Card
+              data={student}
+              tags={[
+                ...chooseRandomTags(student.tech, 3),
+                ...chooseRandomTags(student.industry, 2)
+              ]}
+            />
+          </Link>
         ))}
       </Grid>
     );
