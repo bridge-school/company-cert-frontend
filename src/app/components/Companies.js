50f9c4536276014185b434b51c543472c961ed75
Susie <susie.kim@hotmail.com>
2019-03-16 14:15:07 -0400

    change companies list to card

--- src/app/components/Companies.js
+++ src/app/components/Companies.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
 import getCompaniesData from '../store/actions/getCompaniesData';
-import CompanyCard from './CompanyCard';
+import Card from './Card';
+import { Link } from 'react-router-dom';
 import SecondaryNav from './SecondaryNav';
 
 class Companies extends React.Component {
@@ -12,25 +13,37 @@ class Companies extends React.Component {
   }
 
   renderCompaniesList() {
-    const { companies } = this.props;
+    const { companies, checklist, showOnlyCertified } = this.props;
     return companies.map(company => {
-      return <CompanyCard key={company.id} {...company} />;
+      return (
+        <Link
+          to={`companies/${company.id}`}
+          key={company.id}
+          style={{ display: showOnlyCertified && company.score < 6 ? 'none' : 'block' }}
+        >
+          <Card data={company} total={checklist.length} />
+        </Link>
+      );
     });
   }
 
   render() {
     return (
-      <Grid item xs={12}>
+      <Fragment>
         <SecondaryNav />
-        <div style={{ marginTop: '120px' }}>{this.renderCompaniesList()}</div>
-      </Grid>
+        <Grid item xs={10} sm={8} md={6}>
+          <div style={{ marginTop: '120px' }}>{this.renderCompaniesList()}</div>
+        </Grid>
+      </Fragment>
     );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    companies: state.companies.data
+    companies: state.companies.data,
+    checklist: state.frontendData.checklist,
+    showOnlyCertified: state.companies.showOnlyCertified
   };
 };
 
