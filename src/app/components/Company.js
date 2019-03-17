0a4651ce53284207c121fecfb5a9fe969e38ca0f
Susie <susie.kim@hotmail.com>
2019-03-17 16:05:05 -0400

    remove score prop

--- src/app/components/Company.js
+++ src/app/components/Company.js
@@ -101,7 +101,7 @@ class Company extends React.Component {
         </Typography>
         {companyMatches.map(match => (
           <Link to={`/students/${match.id}`} key={match.id}>
-            <Card data={match} total={checklist.length} tags={[...match.tech, ...match.industry]} />
+            <Card data={match} tags={[...match.tech, ...match.industry]} />
           </Link>
         ))}
       </Grid>
